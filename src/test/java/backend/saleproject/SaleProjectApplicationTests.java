76dca7043d578e13d7c273fc1ec7c43abe0c2ddc
André Troise Agostinis <andre.t.agostinis@gmail.com>
2024-05-24 11:54:21 -0300

    Upando a main e classe Test

---
+++ src/test/java/backend/saleproject/SaleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.saleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
